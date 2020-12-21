a9c255a1cd77ac15fcf18ff9e1270ed7243c6e4c
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-12-21 13:39:41 +0200

    Add app config

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 
+const config = require('./config/app')
+
 const app = express();
 
-const port = process.env.APP_PORT;
+const port = config.appPort;
 
 app.get('/', (req, res) => {
   return res.send('Home page');
