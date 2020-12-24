1d1c12e8adba3a95b40a1546af00a1e9de0abcc7
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-12-24 22:08:41 +0200

    Add axios base configuration  - Add API services  - Add CORS

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import AuthService from '../../services/authService'
 import loginImage from '../../assets/images/login.svg';
 import './Auth.scss';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    AuthService.login({email, password}).then(res => console.log(res))
+   /* axios.post(`http://localhost:3002/login`, {email, password})
+      .then(res => {
+        console.log('res => ', res)
+      })
+      .catch(err => {
+        console.log('err => ', err)
+      })*/
+
+    console.log({email, password})
+  }
+  
   return (
     <div id='auth-container'>
       <div id='auth-card'>
@@ -13,16 +32,24 @@ const Login = () => {
           </div>
           <div id='form-section'>
             <h2>Welcome back</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className='input-field'>
-                <input type="email" placeholder='Email'/>
+                <input value={email}
+                       onChange={e => setEmail(e.target.value)}
+                       required="required"
+                       type="email"
+                       placeholder='Email'/>
               </div>
               <div className='input-field'>
-                <input type="text" placeholder='Password'/>
+                <input value={password}
+                       onChange={e => setPassword(e.target.value)}
+                       required="required"
+                       type="text"
+                       placeholder='Password'/>
               </div>
               <button>LOGIN</button>
             </form>
-              <p>Don`t have an account? <Link to='/register' style={{ textDecoration: 'none' }}>Register</Link></p>
+            <p>Don`t have an account? <Link to='/register' style={{ textDecoration: 'none' }}>Register</Link></p>
           </div>
         </div>
       </div>
