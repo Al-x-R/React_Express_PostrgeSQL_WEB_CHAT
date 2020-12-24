ddcfd5f31912d033d8609e2249f29a41121f6862
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-12-24 14:55:48 +0200

    Add validation rules for login and registration with express-validator

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,8 +1,11 @@
 const authRouter = require('express').Router();
 const { login, register } = require('../controllers/authController');
+const { validate } = require('../validators');
+const { rules: registrationRules } = require('../validators/auth/register');
+const { rules: loginRules } = require('../validators/auth/login');
 
-authRouter.post('/login', login);
+authRouter.post('/login', [loginRules, validate], login);
 
-authRouter.post('/register', register);
+authRouter.post('/register', [registrationRules, validate], register);
 
 module.exports = authRouter;
