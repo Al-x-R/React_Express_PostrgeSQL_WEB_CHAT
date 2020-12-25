5779406d1210742f8832a36797f802355c0750f4
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-12-25 21:44:10 +0200

    Add protected route component

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -3,12 +3,13 @@ import {Switch, Route} from 'react-router-dom'
 import Login from '../components/Auth/Login';
 import Chat from '../components/Chat/Chat';
 import Register from '../components/Auth/Register';
+import ProtectedRoute from '../components/Route/ProtectedRoute';
 
 
 export default () => {
   return (
     <Switch>
-      <Route exact path='/' component={Chat} />
+      <ProtectedRoute exact path='/' component={Chat} />
       <Route path='/login' component={Login} />
       <Route path='/register' component={Register} />
       <Route render={() => <h1>404 page not found</h1>} />
