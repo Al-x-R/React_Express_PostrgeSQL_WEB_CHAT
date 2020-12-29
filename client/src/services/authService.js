2b7104f235e4fcb416446e746f987a792f16f231
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-12-29 16:07:03 +0200

    Add user and token to local storage

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -5,26 +5,36 @@ const AuthService = {
   login: (data) => {
     return API.post('/login', data)
       .then(({ data }) => {
-        API.defaults.headers['Authorization'] = `Bearer ${data.token}`;
+        setHeadersToLocalStorage(data);
         return data;
       })
       .catch(err => {
         console.log('Auth service error', err);
       });
   },
+
   register: (data) => {
     return API.post('/register', data)
       .then(({ data }) => {
-        API.defaults.headers['Authorization'] = `Bearer ${data.token}`;
+        setHeadersToLocalStorage(data);
         return data;
       })
       .catch(err => {
         console.log('Auth service error', err);
       });
   },
+
   logout: () => {
     API.defaults.headers['Authorization'] = '';
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
   },
+};
+
+const setHeadersToLocalStorage = ({ user, token }) => {
+  API.defaults.headers['Authorization'] = `Bearer ${token}`;
+  localStorage.setItem('user', JSON.stringify(user));
+  localStorage.setItem('token', token);
 };
 
 export default AuthService;
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,14 +1,14 @@
 const { User } = require('../models');
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const config = require('../config/app')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('../config/app');
 
 const generateToken = (user) => {
-  delete user.password
+  delete user.password;
 
-  const token = jwt.sign(user, config.appKey, {expiresIn: config.tokenExpiresIn})
-  return {...user, ...{ token }}
-}
+  const token = jwt.sign(user, config.appKey, { expiresIn: config.tokenExpiresIn });
+  return { ...{ user }, ...{ token } };
+};
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
@@ -19,27 +19,27 @@ exports.login = async (req, res) => {
     });
 
     if (userInstance && bcrypt.compareSync(password, userInstance.password)) {
-      const userWithToken = generateToken(userInstance.get({raw: true}))
-      userWithToken.avatar = userInstance.avatar
+      const userWithToken = generateToken(userInstance.get({ raw: true }));
+      userWithToken.user.avatar = userInstance.avatar;
 
-      return res.status(200).send( userWithToken )
+      return res.status(200).send(userWithToken);
     }
 
-    return res.status(404).send({message: 'Incorrect password or email'})
+    return res.status(404).send({ message: 'Incorrect password or email' });
 
   } catch (e) {
-    return res.status(500).json({message: e.message})
+    return res.status(500).json({ message: e.message });
   }
 };
 
 exports.register = async (req, res) => {
   try {
-    const {body} = req
-    const userInstance = await User.create(body)
-    const userWithToken = generateToken(userInstance.get({raw: true}))
+    const { body } = req;
+    const userInstance = await User.create(body);
+    const userWithToken = generateToken(userInstance.get({ raw: true }));
 
-    return res.status(201).send( userWithToken )
+    return res.status(201).send(userWithToken);
   } catch (e) {
-    return res.status(409).json({message: 'User with this mail already exists'})
+    return res.status(409).json({ message: 'User with this mail already exists' });
   }
 };
