c49efb354b350d457feb525ec4e5e05c0ffaf5a1
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-12-29 15:32:12 +0200

    Add logout

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,30 +1,30 @@
-import API from './api'
+import API from './api';
 
 const AuthService = {
 
   login: (data) => {
     return API.post('/login', data)
-      .then(({data}) => {
-        API.defaults.headers['Authorization'] = `Bearer ${data.token}`
-        return data
+      .then(({ data }) => {
+        API.defaults.headers['Authorization'] = `Bearer ${data.token}`;
+        return data;
       })
       .catch(err => {
-        console.log('Auth service error', err)
-      })
+        console.log('Auth service error', err);
+      });
   },
   register: (data) => {
     return API.post('/register', data)
-      .then(({data}) => {
-        API.defaults.headers['Authorization'] = `Bearer ${data.token}`
-        return data
+      .then(({ data }) => {
+        API.defaults.headers['Authorization'] = `Bearer ${data.token}`;
+        return data;
       })
       .catch(err => {
-        console.log('Auth service error', err)
-      })
+        console.log('Auth service error', err);
+      });
   },
   logout: () => {
+    API.defaults.headers['Authorization'] = '';
+  },
+};
 
-  }
-}
-
-export default AuthService
+export default AuthService;
