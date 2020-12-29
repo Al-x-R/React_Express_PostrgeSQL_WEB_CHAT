c10a58e82fe0c4c124d1c3bfe2a7fc02e09f59d1
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-12-29 15:31:52 +0200

    Add drop down menu

--- client/src/components/Chat/components/Navbar/Navbar.jsx
+++ client/src/components/Chat/components/Navbar/Navbar.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {logout} from '../../../../store/actions/auth'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Navbar.scss';
 
 const Navbar = () => {
   const user = useSelector(state => state.authReducer.user);
+  const dispatch = useDispatch()
+  const [showProfileOptions, setShowProfileOptions] = useState(false);
 
   return (
     <div id='navbar'>
       <h2>Chat.io</h2>
-      <div id='profile-menu'>
+      <div id='profile-menu'
+           onClick={() => setShowProfileOptions(!showProfileOptions)}>
         <img width="40" height="40" src={user.avatar} alt="Avatar"/>
         <p>{user.firstName} </p>
         <FontAwesomeIcon icon='caret-down' className='fa-icon'/>
+        {
+          showProfileOptions &&
+          <div id='profile-options'>
+            <p>Update profile</p>
+            <p onClick={() => dispatch(logout())}>Logout</p>
+          </div>
+        }
       </div>
     </div>
   );
