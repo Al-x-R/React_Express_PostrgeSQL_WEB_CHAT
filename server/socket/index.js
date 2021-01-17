511e08e46d796abaca8e115430d8c4cf2e0bb1b5
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-01-17 15:57:48 +0200

    Add get chatters query

--- server/socket/index.js
+++ server/socket/index.js
@@ -2,17 +2,87 @@ const socketIo = require('socket.io');
 const { sequelize } = require('../models');
 const Message = require('../models').Message;
 
+const users = new Map();
+const userSockets = new Map();
+
 const SocketServer = (server) => {
   const io = socketIo(server);
 
   io.on('connection', (socket) => {
 
     socket.on('join', async (user) => {
-      console.log('New user joined', user.firstName);
-      io.to(socket.id).emit('typing', 'User typing...')
-    });
-  });
+
+      let sockets = [];
+
+      if (users.has(user.id)) {
+        const existingUser = users.get(user.id);
+        existingUser.sockets = [...existingUser.sockets, ...[socket.id]];
+        users.set(user.id, existingUser);
+        sockets = [...existingUser.sockets, ...[socket.id]];
+        userSockets.set(socket.id, user.id);
+      } else {
+        users.set(user.id, { id: user.id, sockets: [socket.id] });
+        sockets.push(socket.id);
+        userSockets.set(socket.id, user.id);
+      }
+
+      const onlineFriends = []; // ids
+
+      const chatters = await getChatters(user.id); // query
+
+      console.log(chatters)
+
+      // notify his friends that user is now online
+      for (let i = 0; i < chatters.length; i++) {
+        if (users.has(chatters[i])) {
+          const chatter = users.get(chatters[i]);
+          chatter.sockets.forEach(socket => {
+            try {
+              io.to(socket).emit('online', user);
+            } catch (e) {
+              console.log(e);
+            }
+          });
+          onlineFriends.push(chatter.id);
+        }
+      }
+
+      // send to user sockets which of his friends are online
+      sockets.forEach(socket => {
+        try {
+          io.to(socket).emit('friends', onlineFriends);
+        } catch (e) {
+          console.log(e);
+        }
+      });
+    }); // join socket
+
+  }); // connection
 
 };
+
+const getChatters = async (userId) => {
+  try {
+
+    const [results, metadata] = await sequelize.query(`
+        select "cu"."userId" from "ChatUsers" as cu
+        inner join (
+            select "c"."id" from "Chats" as c
+            where exists (
+                select "u"."id" from "Users" as u
+                inner join "ChatUsers" on u.id = "ChatUsers"."userId"
+                where u.id = ${parseInt(userId)} and c.id = "ChatUsers"."chatId"
+            )
+        ) as cjoin on cjoin.id = "cu"."chatId"
+        where "cu"."userId" != ${parseInt(userId)}
+    `)
+
+    return results.length > 0 ? results.map(el => el.userId) : []
+
+  } catch (e) {
+    console.log(e);
+    return []
+  }
+}
 
 module.exports = SocketServer;
