cfe1f3936b01191d67234866499d0beeb48d6d9a
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-01-19 13:08:30 +0200

    Refactor socket connect

--- client/src/components/Chat/hooks/socketConnect.js
+++ client/src/components/Chat/hooks/socketConnect.js
@@ -1,34 +1,38 @@
 import { useEffect } from 'react';
 import socketIOClient from 'socket.io-client';
-import {onlineFriends, onlineFriend, offlineFriend} from '../../../store/actions/chat'
+import { fetchChats, onlineFriends, onlineFriend, offlineFriend } from '../../../store/actions/chat';
 
 function useSocket(user, dispatch) {
 
   useEffect(() => {
+    dispatch(fetchChats())
+      .then(res => {
+        const socket = socketIOClient.connect('http://localhost:3000');
 
-    const socket = socketIOClient.connect('http://localhost:3000');
+        socket.emit('join', user);
 
-    socket.emit('join', user);
+        socket.on('typing', (user) => {
+          console.log('Event', user);
+        });
 
-    socket.on('typing', (user) => {
-      console.log('Event', user)
-    })
+        socket.on('friends', (friends) => {
+          console.log("Friends", friends);
+          dispatch(onlineFriends(friends));
+        });
 
-    socket.on('friends', (friends) => {
-      console.log("Friends", friends);
-      dispatch(onlineFriends(friends))
-    })
+        socket.on('online', (user) => {
+          console.log("Online", user);
+          dispatch(onlineFriend(user));
+        });
 
-    socket.on('online', (user) => {
-      console.log("Online", user);
-      dispatch(onlineFriend(user))
-    })
+        socket.on('offline', (user) => {
+          console.log("Offline", user);
+          dispatch(offlineFriend(user));
+        });
 
-    socket.on('offline', (user) => {
-      console.log("Offline", user);
-      dispatch(offlineFriend(user))
-    })
-
+        console.log(res);
+      })
+      .catch(err => console.log(err));
   }, [dispatch]);
 }
 
