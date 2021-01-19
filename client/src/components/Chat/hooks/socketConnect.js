416cabbb0067db8ca9b38343d14d531eb9aa8d3b
Alexander Roshchin <alexander.roshchin85@gmail.com>
2021-01-19 12:54:30 +0200

    Add updating user status ( online / offline )  - add actions  - add reducers  - add sockets

--- client/src/components/Chat/hooks/socketConnect.js
+++ client/src/components/Chat/hooks/socketConnect.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import socketIOClient from 'socket.io-client';
+import {onlineFriends, onlineFriend, offlineFriend} from '../../../store/actions/chat'
 
 function useSocket(user, dispatch) {
 
@@ -12,6 +13,22 @@ function useSocket(user, dispatch) {
     socket.on('typing', (user) => {
       console.log('Event', user)
     })
+
+    socket.on('friends', (friends) => {
+      console.log("Friends", friends);
+      dispatch(onlineFriends(friends))
+    })
+
+    socket.on('online', (user) => {
+      console.log("Online", user);
+      dispatch(onlineFriend(user))
+    })
+
+    socket.on('offline', (user) => {
+      console.log("Offline", user);
+      dispatch(offlineFriend(user))
+    })
+
   }, [dispatch]);
 }
 
